633f385357ccc6a67d9d9cd9bca1d4ab51e21844
rifat32 <rifatbilalphilips@gmail.com>
2022-12-24 21:59:17 +0600

    auto deploy try

--- app/Http/Controllers/UserManagementController.php
+++ app/Http/Controllers/UserManagementController.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-// ccccccc
+// ddddddd
 class UserManagementController extends Controller
 {
     use ErrorUtil;
