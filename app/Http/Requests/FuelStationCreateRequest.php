45cd59945a21a98bf9f5be262dd46f7afd92bb8b
rifat32 <rifatbilalphilips@gmail.com>
2023-02-14 22:14:43 +0600

    garage times half done

--- app/Http/Requests/FuelStationCreateRequest.php
+++ app/Http/Requests/FuelStationCreateRequest.php
@@ -27,8 +27,16 @@ class FuelStationCreateRequest extends FormRequest
         return [
             "name" => "required|string",
             "address" => "required|string",
-            "opening_time"=> ['required','date_format:H:i:s?', new TimeValidation
-        ],
+            "opening_time"=> ['required','date_format:H:i:s?', // Ensure that the input is in the H:i:s format
+            function ($attribute, $value, $fail) {
+                $timeParts = explode(':', $value);
+                $hour = $timeParts[0];
+                $minute = $timeParts[1];
+                $second = !empty($timeParts[2])?$timeParts[2]:0;
+                if (!checkdate(1, 1, 1) || !checkdate(1, 1, 1970) || $hour < 0 || $hour > 23 || $minute < 0 || $minute > 59 || $second < 0 || $second > 59) {
+                    $fail('The '.$attribute.' field must be a valid time value.');
+                }
+            }],
             "closing_time" => ['required','date_format:H:i:s?', new TimeValidation
             ],
             "description" => "nullable|string",
