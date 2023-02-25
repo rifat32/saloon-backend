7d28277d58c420cc066df6688cb187ca07b8eb7b
rifat32 <rifatbilalphilips@gmail.com>
2023-02-25 20:53:46 +0600

    app debug false

--- database/migrations/2023_02_19_131543_create_payments_types_table.php
+++ database/migrations/2023_02_19_131543_create_payments_types_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePaymentTypesTable extends Migration
+class CreatePaymentsTypesTable extends Migration
 {
     /**
      * Run the migrations.
