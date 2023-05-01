87243e718e2457ddd42360603cc8bb4ac0bb4d8b
rifat32 <rifatbilalphilips@gmail.com>
2023-05-01 20:58:34 +0600

    model

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Shop extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "name",
+        "about",
+        "web_page",
+        "phone",
+        "email",
+        "additional_information",
+        "address_line_1",
+        "address_line_2",
+        "lat",
+        "long",
+        "country",
+        "city",
+        "postcode",
+        "logo",
+        "status",
+        // "is_active",
+        "is_mobile_shop",
+        "wifi_available",
+        "labour_rate",
+        "average_time_slot",
+        "owner_id",
+        "created_by",
+    ];
+
+    public function owner(){
+        return $this->belongsTo(User::class,'owner_id', 'id');
+    }
 }
